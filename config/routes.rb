8bdec06575069829af67d6ba5aba52ebcccc547b
Erika <erika.yyma@gmail.com>
2019-10-25 09:41:46 +0000

    made model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'user/new'
   get 'pages/help'
   get 'pages/index'
   root 'pages#index'
+  
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
