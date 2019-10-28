aa672abb05d589988949ee039810fb75f2c6d5fa
Erika <erika.yyma@gmail.com>
2019-10-28 09:51:30 +0000

    display error message

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'pages/help'
-  get 'pages/index'
   root 'pages#index'
   
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
