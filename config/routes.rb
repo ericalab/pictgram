852160a6ae10c99948b405bbe50b48df0ad33000
Erika <erika.yyma@gmail.com>
2019-11-01 08:06:51 +0000

    made session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'pages/help'
   root 'pages#index'
   
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get '/login', to:'sessions#new'
+  post '/login', to:'sessions#create'
+  delete 'logout', to:'sessions#destroy'
 end
 
 
