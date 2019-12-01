7e1b57f8fc88c9f8ade9cfa902e8d35ebe98c0f5
Erika <erika.yyma@gmail.com>
2019-12-01 03:43:56 +0000

    Changed Show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,92 @@ class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
   helper_method :current_user, :logged_in?
 
+  # 松田追加
+  helper_method :nilOrEmpty?, :getScrTopFromParameter, :setScrTop2Url
+
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
-  
+
   def logged_in?
-    !@current_user.nil?
+    !current_user.nil?
+  end
+
+  # 松田追加
+  # nilか空文字かの真偽値を返す
+  # @param [object] 対象のobject
+  # @return [string] 画面表示位置をパラメータとして渡すリンクのhtmlタグ
+  def nilOrEmpty?(obj)
+    return obj.nil? || obj.empty?
+  end
+
+  # 画面表示位置固定用パラメータ名を返す
+  # @param [nil]
+  # @return [string] 画面表示位置固定用パラメータ名
+  def getScrTopParamName
+    return view_context.scrTopParamName
+  end
+  def getScrTopParamNameAsSym
+    view_context.scrTopParamName.to_sym
+  end
+
+  # 一覧画面表示位置固定用パラメータ名を返す
+  # @param [nil]
+  # @return [string] 一覧画面表示位置固定用パラメータ名
+  def getLstScrTopParamName
+    return view_context.lstScrTopParamName
+  end
+  def getLstScrTopParamNameAsSym
+    view_context.lstScrTopParamName.to_sym
+  end
+
+  # パラメータから画面表示位置を取得する
+  # パラメータに該当値がない場合は、0を返す
+  # @param [nil]
+  # @return [integer] 画面表示位置
+  def getScrTopFromParameter
+    scrtop = 0
+    if !nilOrEmpty?(params[getScrTopParamNameAsSym])
+      scrtop = params[getScrTopParamNameAsSym]
+    end
+    return scrtop
+  end
+
+  # パラメータから一覧画面表示位置を取得する
+  # パラメータに該当値がない場合は、0を返す
+  # @param [nil]
+  # @return [integer] 画面表示位置
+  def getLstScrTopFromParameter
+    lstScrtop = 0
+    if !nilOrEmpty?(params[getLstScrTopParamNameAsSym])
+      lstScrtop = params[getLstScrTopParamNameAsSym]
+    end
+    return lstScrtop
+  end
+
+  # パラメータから画面表示位置を取得し、urlにパラメータとして加える
+  # @param [nil]
+  # @return [string] 画面表示位置を追記したurl
+  def setScrTop2Url(url, paramName, value)
+    path =  url + '/?' + paramName + '=' + value.to_s
+    return path
+  end
+
+  # パラメータから画面表示位置を取得し、gonのscrtopに設定する
+  # パラメータに該当値がない場合は、0が設定される
+  # @param [nil]
+  # @return [nil]
+  def hiddenTag(className, value)
+    html = '<input type="hidden" class="' + className + '" name ="' + className + '" value="' + value.to_s + '"/>'
+    return html
+  end
+
+  # トピック詳細ページ用のインスタンス変数を設定する
+  def setAllInstanceValForTopicShow
+    # 一覧の表示位置をpostで持ち回る
+    @lstScrTop = getLstScrTopFromParameter
+    @scrtop = getScrTopFromParameter
+    @topic_id = params[:topic_id]
   end
 end
+
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -25,11 +25,54 @@ class TopicsController < ApplicationController
     @topic =Topic.find_by(id: params[:id])
     @user = @topic.user
     @comment = Comment.new
+    
+    respond_to do |format|
+      format.html {
+
+          
+          @topic.comments.each do |comment|
+            comment.content = compile_lines(comment)
+          end
+      
+          @comments = @topic.comments
+      
+          lastCommentId = 0
+          
+          if @topic.comments.count > 0
+            lastCommentId = @topic.comments.last.id;
+          end
+          
+          render :show
+      }
+
+      # json形式でアクセスがあった場合は、params[:message][:id]よりも大きいidがないかMessageから検索して、@new_messageに代入する
+      format.json {
+        newcomments = Comment.where('id > ? AND topic_id = ?', params[:comment][:id], params[:comment][:topic_id])
+        newcomments.each do |comment|
+          comment.content = compile_lines(comment)
+        end
+        @new_comment = newcomments
+      }
+    end
   end 
   
   private
   def topic_params
     params.require(:topic).permit(:image, :description)
+  end
+  
+  def compile_lines(comment)
+    lines = comment.user.name + '： ' + comment.content
+    # Admin 又は ログインユーザのコメントには削除リンクを追加
+    if current_user.id == comment.user_id
+        lines += '<a href="/comments/delete?'
+        lines += 'id=' + comment.id.to_s
+        #lines += '&' + getLstScrTopParamName + '=' + @lstScrTop.to_s
+        #lines += '&' + getScrTopParamName + '=' + @scrtop.to_s
+        lines += '&' + 'topic_id' + '=' + @topic_id.to_s
+        lines += '"><span style="color:#ff0000;">　×</span></a>'
+    end
+    return lines
   end
   
 end
